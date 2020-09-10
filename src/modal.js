9264375e185922d4d188717975a46e3a46d26a4c
nykiway <nicoleoliviawiehe@gmail.com>
2020-09-10 12:36:37 -0600

    Design Modal

--- src/modal.js
+++ src/modal.js
@@ -1,27 +1,13 @@
-// Left Modal:
+let rightModal = document.querySelector(".modal-right");
+let rightBtn = document.querySelector(".dial-right");
+let rightSpan = document.querySelector(".close-right");
 
-// Get the modal
-var leftModal = document.getElementById("myModal-left");
-
-// Get the button that opens the modal
-var btn = document.getElementById("dial-left");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-left")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  leftModal.style.display = "block";
+rightBtn.onclick = function () {
+  rightModal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  leftModal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == leftModal) {
-    leftModal.style.display = "none";
+  if (event.target == rightModal) {
+    rightModal.style.display = "none";
   }
 };
